65ee36415f8fa4f00f8fe090e26e943169e89370
Anton <Osipenko>
2021-02-04 10:50:10 +0300

    about Promise

--- resources/js/testDraft.js
+++ resources/js/testDraft.js
@@ -1,16 +1,58 @@
-const person = {
- name: 'Pop',
- age: 30,
- lastName: {
-    nameL: 'KEKLOL'
- }
+/*
+console.log('загрузка данных');
+
+//resolve успешно
+//reject ошибка
+const prm = new Promise((resolve, reject) => {
+
+   setTimeout(() => {
+      console.log('подготовка данных...');
+
+      const product = {
+         name: 'TV',
+         price: 2000
+      };
+
+   resolve(product);
+   }, 2000);
+
+});
+
+prm.then((product) =>{
+   new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+         product.status = 'order';
+         resolve(product);
+      }, 2000);
+   
+}).then(product => {
+   product.modify = true;
+   return product;
+}).then(() => {
+   console.log(product);
+}).catch(() =>{
+   console.log('ошибка');
+});
+
+
+});
+*/
+
+const test = time => {
+   return new Promise(resolve => {
+      setTimeout(() =>{
+         resolve();
+      }, time);
+   });
 };
 
-//console.log(JSON.parse(JSON.stringify(person))); // конверт к json(stringify) из (parse)
+//ждем окончание всех промисов
+Promise.all([test(1000), test(2000)]).then(() =>{
+      console.log('all');
+});
 
-//глубокое копирование объекта не зависимого от person, при изменении
-const clone = JSON.parse(JSON.stringify(person));
-clone.lastName.nameL = 'LOLKEK';
-
-console.log(person);
-console.log(clone);
+//ждем самый быстры пропис и делаем какие то действия 
+Promise.race([test(1000), test(2000)]).then(() =>{
+   console.log('race');
+});
